efc15759d2989a01a2ad3fde47cd5f6515b95f78
Robin Ricard <ricard.robin@gmail.com>
2014-10-20 11:03:32 +0200

    Add lib structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var ddgScrapper = require('../lib/ddg-scrapper.js');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Form' });
+});
+
+/* POST home page */
+router.get('/search', function(req, res) {
+  res.render('index', { title: 'Results' });
 });
 
 module.exports = router;
