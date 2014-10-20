b3ad83719f92ebd7246e3423d72643a69abc1724
Robin Ricard <ricard.robin@gmail.com>
2014-10-20 11:42:08 +0200

    Scrapper example

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
+
 
 var ddgScrapper = require('../lib/ddg-scrapper.js');
 
-router.get('/', function(req, res) {
-  res.render('index', { query: req.query.q });
+router.get('/', function(req, res, next) {
+  if(!req.query.q) {
+    res.render('index');
+    return next();
+  }
+  async.waterfall([
+    function getQuery(cb) {
+      cb(null, req.query.q);
+    },
+    ddgScrapper,
+    // textExtracter,
+    // rdfExtracter,
+    // ???,
+    function renderResults(results, cb) {
+      console.log(results);
+      res.render('index', { query: req.query.q, results: results });
+    }
+  ], next);
 });
 
 module.exports = router;
