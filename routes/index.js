f749ad37ea9b7d16b76614acf30283a4e12d3787
Robin Ricard <ricard.robin@gmail.com>
2014-11-04 17:59:14 +0100

    Refactor

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var async = require('async');
 
 
 var ddgScrapper = require('../lib/ddg-scrapper.js');
-var textExtracter = require('../lib/text-extracter.js');
-var rdfExtracter = require('../lib/rdf-extracter.js');
+var textExtracter = require('../lib/each/text-extracter.js');
+var rdfExtracter = require('../lib/each/rdf-extracter.js');
 
 router.get('/', function(req, res, next) {
   if(!req.query.q) {
@@ -19,31 +19,16 @@ router.get('/', function(req, res, next) {
       cb(null, req.query.q);
     },
     ddgScrapper,
-    function extractTextEach(htmls, cb) {
-      var texts = {};
-      async.each(Object.keys(htmls), function mapHtmls(uri, cbE) {
-        var text = textExtracter(htmls[uri]);
-        if(text) {
-          texts[uri] = text;
-        }
-        cbE();
-      }, function(err) {
-        cb(err, texts);
-      });
-    },
-    function rdfEach(texts, cb) {
-      var graphs = {};
-      async.each(Object.keys(texts), function mapTexts(uri, cbE) {
-        rdfExtracter(uri, texts[uri], function(err, graph) {
-          if(err && !graph) {
-            return cbE(err);
-          }
-          graphs[uri] = graph;
-          cbE();
-        });
-      }, function(err) {
-        cb(err, graphs);
-      });
+    function(pages, cb) {
+      async.map(pages, function mapPages(page, cbMap) {
+        async.waterfall([
+          function getPage(cbGet) {
+            cbGet(null, pages);
+          },
+          textExtracter,
+          rdfExtracter
+        ], cbMap);
+      }, cb);
     },
     // ???,
     function renderResults(results, cb) {
