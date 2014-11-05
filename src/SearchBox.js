e2fe9f4b81245a8f7f1c091c8a7d99912312d2f2
Robin Ricard <ricard.robin@gmail.com>
2014-11-05 03:34:43 +0100

    Fix

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,6 +4,9 @@ var SearchBox = React.createClass({
   handleSubmit: function() {
     this.props.onSearchSubmit(this.refs.query.getDOMNode().value.trim());
   },
+  componentWillReceiveProps: function(props) {
+    this.refs.query.getDOMNode().value = props.query;
+  },
   render: function() {
     return (
       <div className="navbar navbar-default">
@@ -12,7 +15,7 @@ var SearchBox = React.createClass({
             Culture View
           </span>
           <form className="navbar-left col-md-8" onSubmit={this.handleSubmit} action="#">
-            <input value={this.props.query} type="text" size="100%" className="form-control" placeholder="Type an Artist, a Movie, ... and hit ENTER !" ref="query"/>
+            <input type="text" size="100%" className="form-control" placeholder="Type an Artist, a Movie, ... and hit ENTER !" ref="query"/>
           </form>
         </div>
       </div>
